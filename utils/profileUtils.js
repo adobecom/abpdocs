97d11e8923801fe14c69bbf2277eb08302dcf25d
Sekhar Babu Karri <sbabukarri@adobe.com>
2023-05-16 13:40:36 +0100

    feat(reviewsession): fix to resolve the session path

--- utils/profileUtils.js
+++ utils/profileUtils.js
@@ -1,14 +1,14 @@
-import { getSessionStorage, setSessionStorage } from './sessionStorageUtils.js';
+import sessionStorageUtils from './sessionStorageUtils.js';
 
 const PROFILE_URL = '/services/profile';
 
 const loadProfileDetails = async () => {
-  let profileData = getSessionStorage('profile');
+  let profileData = sessionStorageUtils.getSessionStorage('profile');
   if (!profileData) {
     try {
       const response = await fetch(PROFILE_URL, { headers: { 'Content-Type': 'application/json' } });
       profileData = await response.json();
-      setSessionStorage('profile', profileData?.details);
+      sessionStorageUtils.setSessionStorage('profile', profileData?.details);
     } catch (e) {
       console.log('error in fetching the user data', e);
     }
