ba886195836cd35e9f985709db8e56b0c77bbc86
Sekhar Babu Karri <sbabukarri@adobe.com>
2023-06-07 10:25:04 +0100

    feat(ABPDOCS-201): domain name changes

--- blocks/tree-view/tree-view.js
+++ blocks/tree-view/tree-view.js
@@ -1,6 +1,6 @@
 import { getLibs } from '../../scripts/utils.js';
 
-const BACOM_HOSTS = ['localhost', '--abpdocs--adobecom.hlx.page', '--abpdocs--adobecom.hlx.live', 'business.adobe.com', 'abp.adobe.com'];
+const BACOM_HOSTS = ['localhost', '--abpdocs--adobecom.hlx.page', '--abpdocs--adobecom.hlx.live', 'abp.adobe.com'];
 
 export const isCurrentPage = (link) => {
   const currentPath = window.location.pathname.replace('.html', '');
